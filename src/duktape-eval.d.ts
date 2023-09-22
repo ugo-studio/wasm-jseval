0c369dc069827d45854d7131d9ecfe88b8034604
ugo-studio <ugo.code.studio@gmail.com>
2023-09-22 15:32:07 +0100

    .

--- src/duktape-eval.d.ts
+++ src/duktape-eval.d.ts
@@ -4,6 +4,7 @@ export interface DukTapeModule {
     cwrap: function (name, res, param): function (js_code): any,
 }
 /**
+* @param {Object={}} moduleArgs
 * @returns {DukTapeModule}
 */
-export default function DukTapeModule(): DukTapeModule;
+export default function DukTapeModule(moduleArgs: Object={}): DukTapeModule;
--- src/quickjs-eval.d.ts
+++ src/quickjs-eval.d.ts
@@ -4,6 +4,7 @@ export interface QuickJsModule {
     cwrap: function (name, res, param): function (js_code): any,
 }
 /**
+* @param {Object={}} moduleArgs
 * @returns {QuickJsModule}
 */
-export default function QuickJsModule(): QuickJsModule;
+export default function QuickJsModule(moduleArgs: Object={}): QuickJsModule;
